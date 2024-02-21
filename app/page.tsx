11efeeb5f2e08b23d6ebc8f0ddc5f0a1b1310505
Oprea Catalin <copreapt@gmail.com>
2024-02-21 12:41:25 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { lusitana } from '@/app/ui/fonts';
+
+const HomePage = () => {
+  return (
+    <div className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>HomePage</div>
+  )
+}
+
+export default HomePage
